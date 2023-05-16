5d5ab1f330e0263861cb3b3f68ba4afb167461de
JJungEEun <wjddms0926@gmail.com>
2023-05-16 13:08:24 +0900

    merge

---
+++ src/components/Nav.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="nav white--text">
+      <ul>
+      <b-navbar-nav>
+         <b-nav-item href="/">
+           <font color="white"> &nbsp;&nbsp;메인(임시페이지)</font>
+        </b-nav-item>
+
+        <b-nav-item href="/Project">
+          <img src="./img/theme/project-management.png" height="30px" width="30px" class="d-inline-block align-center" alt="Kitten">
+          <font color="white"> &nbsp;&nbsp;프로젝트 관리</font>
+        </b-nav-item>
+          
+          <b-nav-item href="/Emloyee">
+            <img src="./img/theme/division.png" height="30px" width="30px" class="d-inline-block align-center" alt="Kitten">
+            <font color="white"> &nbsp; 직원 관리</font>
+          </b-nav-item>
+
+          <b-nav-item href="/Evaluation">
+            <img src="./img/theme/satisfaction.png" height="30px" width="30px" class="d-inline-block align-center" alt="Kitten">
+            <font color="white">&nbsp; &nbsp;평가</font>
+          </b-nav-item>
+
+          <b-nav-item href="/Mypage">
+            <img src="./img/theme/avatar.png" height="30px" width="30px" class="d-inline-block align-center" alt="Kitten">
+            <font color="white">&nbsp;마이페이지</font>
+          </b-nav-item>
+      </b-navbar-nav>
+    </ul>
+    </div>
+      <!-- <div> -->
+        <!-- <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="/Project">
+            <img src="./img/theme/icon4.png" height="50px" width="50px" class="d-inline-block align-center" alt="Kitten">
+            프로젝트 관리
+          </b-navbar-brand>
+        </b-navbar>
+
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="/Employee">
+            <img src="./img/theme/icon2.png" height="50px" width="50px" class="d-inline-block align-center" alt="Kitten">
+            직원 관리
+          </b-navbar-brand>
+        </b-navbar>
+
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="/Evaluation">
+            <img src="./img/theme/icon3.png" height="50px" width="30px" class="d-inline-block align-center" alt="Kitten">
+            평가
+          </b-navbar-brand>
+        </b-navbar>
+
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="/Mypage">
+            <img src="./img/theme/icon1.png" class="d-inline-block align-center" alt="Kitten">
+            마이페이지
+          </b-navbar-brand>
+        </b-navbar> -->
+      <!-- </div> -->
+  </template>
+
+     
+<style>
+.nav{
+  width:280px;
+  height: 100%;
+  flex: 4;
+  background-color: #333333 ;  
+}
+
+</style>
+
+<script>
+
+export default {
+  name: "Nav",
+  data() {
+  }
+};
+</script>
