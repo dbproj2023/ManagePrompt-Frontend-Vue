14db7cd33e65ab7b3ea7f56d6aa0be99ab55b9f4
YSBunny <yun0836@naver.com>
2023-05-21 19:32:37 +0900

    포트 3000번 설정, Change_pw, Access, FindAccount 라우터 추가

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,8 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: {
-      "/api": { //'/api'가 들어오면 포트 8082(스프링 서버)로 보낸다
-        target: "http://localhost:8082/api", 
+      "/api": { //'/api'가 들어오면 포트 3000(스프링 서버)로 보낸다
+        target: "http://localhost:3000/api", 
         changeOrigin: true, // cross origin 
         pathRewrite: {
           "^/api": "",
